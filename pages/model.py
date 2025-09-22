989a7cdd4f20f02bf3ba79e11ff45162f9a246d0
Moaz Ahmed Abdel Ghaffar <167555747+Moaz14060@users.noreply.github.com>
2025-09-22 11:19:23 +0300

    Update model.py

--- pages/model.py
+++ pages/model.py
@@ -72,7 +72,7 @@ def train(df):
     st.header("Parameters")
     # Showing the parameters
     st.write("**Intercept (Theta0):**", load_model.intercept_)
-    st.write("**Coefficients (Theta1, Theta2, etc....):**", load_model.coef_")
+    st.write("**Coefficients (Theta1, Theta2, etc....):**", load_model.coef_)
     st.divider()
 
     # Subtitle
@@ -107,4 +107,5 @@ if back:
 if home:
 
     st.switch_page("home.py")
+
 
