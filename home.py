e0624c337c4bd7b2cce2edca1f6dd58e06fad624
Moaz Ahmed Abdel Ghaffar <167555747+Moaz14060@users.noreply.github.com>
2025-09-22 10:57:02 +0300

    Add files via upload

--- home.py
+++ home.py
@@ -6,19 +6,21 @@ st.set_page_config(page_title="Home", page_icon=":material/home:")
 # Main Title
 st.markdown("<h1 style='text-align: center; color: black;'>Welcome</h1>", unsafe_allow_html=True)
 # Text explaining the function of the program
-st.write("In this program we are going to preprocess a data about Students' performance, do some exploratory data analysis and train a linear regression model on it.")
-st.write("Also, we are going to help the user\student to predict the performance based on attributes he\she inputs into the program.")
+st.write("**First,** In this program we are going to preprocess a data about Students' performance, do some exploratory data analysis and train a linear regression model on it.")
+st.write("**Second,** we are going to help the user\student to predict the performance based on attributes he\she inputs into the program.")
+st.write("**Third,** create a recommendation system that triggers when the predicted value is below a certain threshold based on the given factors' values.")
 st.divider()
 # Explaining the data set
 st.header("Features:")
-st.write("**Hours Studied:** The total number of hours spent studying by each student.")
-st.write("**Previous Scores:** The scores obtained by students in previous tests.")
-st.write("**Extracurricular Activities:** Whether the student participates in extracurricular activities (Yes or No).")
-st.write("**Sleep Hours:** The average number of hours of sleep the student had per day.")
-st.write("**Sample Question Papers Practiced:** The number of sample question papers the student practiced.")
+st.write("**1. Hours Studied:** The total number of hours spent studying by each student.")
+st.write("**2. Previous Scores:** The scores obtained by students in previous tests.")
+st.write("**3. Extracurricular Activities:** Whether the student participates in extracurricular activities (Yes or No).")
+st.write("**4. Sleep Hours:** The average number of sleep hours the student had per day.")
+st.write("**5. Sample Question Papers Practiced:** The number of sample question papers the student practiced.")
 st.header("Target Variable:")
 st.write("**Performance Index:** A measure of the overall performance of each student. The performance index represents the student's " \
-"academic performance and has been rounded to the nearest integer. The index ranges from 10 to 100, with higher values indicating better performance.")
+"academic performance and has been rounded to the nearest integer. The index ranges from 10 to 100, with higher values " \
+"indicating better performance and lower values indecating otherwise.")
 st.divider()
 
 # To control the buttons' positions
@@ -27,5 +29,4 @@ with col3:
     prep_eda = st.button("Preprocessing and EDA", type="primary")
 # Once the button pressed move to the next page
 if prep_eda:
-
     st.switch_page("pages/preprocess_eda.py")
